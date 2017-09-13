275636c38b251fcf099a698698733782599fad98
unknown <crimsonmail55@yahoo.com>
2017-09-13 16:34:43 -0400

    Added form and models

---
+++ SteamRecycle/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SteamRecycle.Models
+{
+    public class Game
+    {
+        public int AppId { get; set; }
+        public string Name { get; set; }
+        public int Playtime { get; set; }
+        public string ImageIcon { get; set; }
+        public string ImageLogo { get; set; }
+        public bool HasVisibleStats { get; set; } = false;
+    }
+}
---
+++ SteamRecycle/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SteamRecycle.Models
+{
+    public class User
+    {
+        public string ProfileId { get; set; }
+        public string Name { get; set; }
+        public int GameCount { get; set; }
+        public IEnumerable<Game> Games { get; set; }
+    }
+}
