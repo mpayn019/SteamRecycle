89c0cf0482fab1cefa0273b22e120ef22f88a239
unknown <crimsonmail55@yahoo.com>
2017-09-13 17:58:11 -0400

    User MVC created

---
+++ SteamRecycle/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SteamRecycle.ViewModels;
+
+namespace SteamRecycle.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User/5
+        public ActionResult Index(int id)
+        {
+            var user = new UserViewModel();
+            user.Id = id;
+
+            return View(user);
+        }
+    }
+}
