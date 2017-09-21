3cf200f7e7845ba9950c77c4c4362c37b9e26c30
unknown <crimsonmail55@yahoo.com>
2017-09-21 14:31:40 -0400

    Added validation to form on main page

--- SteamRecycle/Models/User.cs
+++ SteamRecycle/Models/User.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SteamRecycle.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(17, MinimumLength = 17, ErrorMessage = "SteamID64 must be 17 digits long")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage="SteamID64 must be only numbers")]
         public string ProfileId { get; set; }
-        public string Name { get; set; }
-        public int GameCount { get; set; }
-        public IEnumerable<Game> Games { get; set; }
     }
 }
